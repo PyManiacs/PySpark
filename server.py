33383d789b1b5fe69e82d416e77bbb01028ab22e
SK MAMUN ARFIN <mamunarfin@outlook.com>
2017-08-08 16:05:22 +0530

    removed unecessary functions

--- server.py
+++ server.py
@@ -5,21 +5,21 @@ from pyspark import SparkContext, SparkConf
 
 def init_spark_context():
     # load spark context
-    conf = SparkConf().setAppName("movie_recommendation-server")
+    conf = SparkConf().setAppName("turbine_details-server")
     # IMPORTANT: pass aditional Python modules to each worker
     sc = SparkContext(conf=conf, pyFiles=['engine.py', 'app.py'])
- 
+
     return sc
- 
- 
+
+
 def run_server(app):
- 
+
     # Enable WSGI access logging via Paste
     app_logged = TransLogger(app)
- 
+
     # Mount the WSGI callable object (app) on the root directory
     cherrypy.tree.graft(app_logged, '/')
- 
+
     # Set the configuration of the web server
     cherrypy.config.update({
         'engine.autoreload.on': True,
@@ -27,18 +27,17 @@ def run_server(app):
         'server.socket_port': 8080,
         'server.socket_host': 'localhost'
     })
- 
+
     # Start the CherryPy WSGI web server
     cherrypy.engine.start()
     cherrypy.engine.block()
- 
- 
+
+
 if __name__ == "__main__":
     # Init spark context and load libraries
     sc = init_spark_context()
     dataset_path = os.path.join('datasets', 'ml-latest')
     app = create_app(sc, dataset_path)
- 
+
     # start web server
     run_server(app)
-
